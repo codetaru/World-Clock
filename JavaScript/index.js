da6b9865d6ce5009260c0d01f4b7cc40b868cf7b
codetaru <derrickaballen@gmail.com>
2024-05-18 18:51:15 -0500

    Added redirect

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -35,7 +35,9 @@ citiesElement.innerHTML = `
 </div>
 </div>
 <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
-</div> `;
+</div>
+<a href="/">All Cities</a>
+`;
 }
 
 updateTime();
